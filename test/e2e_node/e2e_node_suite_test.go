8cbe8fc29a1236bebe4594f209b5b5aa6ea3c7a0
Mikhail Mazurskiy <mmazurskiy@atlassian.com>
2018-10-11 21:57:43 +1100

    Seed math/rand in TestMain before tests are executed

--- test/e2e_node/e2e_node_suite_test.go
+++ test/e2e_node/e2e_node_suite_test.go
@@ -75,6 +75,7 @@ func init() {
 }
 
 func TestMain(m *testing.M) {
+	rand.Seed(time.Now().UnixNano())
 	pflag.Parse()
 	framework.AfterReadingAllFlags(&framework.TestContext)
 	os.Exit(m.Run())
@@ -120,7 +121,6 @@ func TestE2eNode(t *testing.T) {
 		return
 	}
 	// If run-services-mode is not specified, run test.
-	rand.Seed(time.Now().UnixNano())
 	RegisterFailHandler(Fail)
 	reporters := []Reporter{}
 	reportDir := framework.TestContext.ReportDir
